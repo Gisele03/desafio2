807363dc0f239df56262abb95647c3d5f47eacc5
Gisele03 <132359841+Gisele03@users.noreply.github.com>
2024-05-23 11:32:12 -0300

    desafio2

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:desafio2/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
